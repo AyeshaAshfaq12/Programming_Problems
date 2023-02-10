4f35d4d9691f38de3772862c541abeb64dff7851
AyeshaAshfaq12 <ayeshaashfaq925@gmail.com>
2023-02-10 08:09:05 +0500

    Create multiples.cpp

---
+++ multiples.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+main()
+{
+ int n;
+ cout<< "Enter the number:";
+ cin>> n;
+ cout<<"Multiples of "<<n<<" are: "<<endl;
+ cout<< n*1 << endl << n*2 << endl << n*3<< endl << n*4 << endl << n*5;
+}
