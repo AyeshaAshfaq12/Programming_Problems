40ccfddf90e06e9987de2d97a9e3807e1a5aa038
AyeshaAshfaq12 <ayeshaashfaq925@gmail.com>
2023-02-11 22:32:23 +0500

    Create sumofmultiples.cpp

---
+++ sumofmultiples.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main()
+{
+ int n, multiples;
+ cout<< "Enter the number:";
+ cin>> n;
+ multiples=(n*1)+(n*2)+(n*3)+(n*4)+(n*5);
+ cout<< "Sum of 1st five multiples of" << n << "is:" << multiples;
+  
+}
