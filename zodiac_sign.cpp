a8476130982ef59569de918ac7f4e204f878fd7e
AyeshaAshfaq12 <ayeshaashfaq925@gmail.com>
2023-02-11 22:32:28 +0500

    Create zodiac_sign.cpp

---
+++ zodiac_sign.cpp
@@ -0,0 +1,108 @@
+#include<iostream>
+using namespace std;
+main()
+{
+string month;
+int date;
+cout<<"Enter your birth date: ";
+cin>>date;
+cout<<"Enter your birth month :";
+cin>>month;
+if(date>=21 && month=="march")
+{
+cout<<"Your Zodiac sign is Aries.";
+}
+if(date<=19 && month=="april")
+{
+cout<<"Your Zodiac sign is Aries.";
+}
+if(date>=20 && month=="april")
+{
+cout<<"Your Zodiac sign is Taurus.";
+}
+if(date<=20 && month=="may")
+{
+cout<<"Your Zodiac sign is Taurus.";
+}
+if(date>=21 && month=="may")
+{
+cout<<"Your Zodiac sign is Gemini.";
+}
+if(date<=20 && month=="june")
+{
+cout<<"Your Zodiac sign is Gemini.";
+}
+if(date>=21 && month=="june")
+{
+cout<<"Your Zodiac sign is Cancer.";
+}
+if(date<=22 && month=="july")
+{
+cout<<"Your Zodiac sign is Cancer.";
+}
+if(date>=23 && month=="july")
+{
+cout<<"Your Zodiac sign is Leo.";
+}
+if(date<=22 && month=="august")
+{
+cout<<"Your Zodiac sign is Leo.";
+}
+if(date>=23 && month=="august")
+{
+cout<<"Your Zodiac sign is Virgo.";
+}
+if(date<=22 && month=="september")
+{
+cout<<"Your Zodiac sign is Virgo.";
+}
+if(date>=23 && month=="september")
+{
+cout<<"Your Zodiac sign is Libra.";
+}
+if(date<=22 && month=="october")
+{
+cout<<"Your Zodiac sign is Libra.";
+}
+if(date>=23 && month=="october")
+{
+cout<<"Your Zodiac sign is Scorpio.";
+}
+if(date<=21 && month=="november")
+{
+cout<<"Your Zodiac sign is Scorpio.";
+}
+if(date>=22 && month=="november")
+{
+cout<<"Your Zodiac sign is Sagittarius.";
+}
+if(date<=21 && month=="december")
+{
+cout<<"Your Zodiac sign is Sagittarius.";
+}
+if(date>=22 && month=="december")
+{
+cout<<"Your Zodiac sign is Capricon.";
+}
+if(date<=19 && month=="january")
+{
+cout<<"Your Zodiac sign is Capricon.";
+}
+if(date>=20 && month=="january")
+{
+cout<<"Your Zodiac sign is Aquarius.";
+}
+if(date<=18 && month=="february")
+{
+cout<<"Your Zodiac sign is Aquarius.";
+}
+if(date>=19 && month=="february")
+{
+cout<<"Your Zodiac sign is Pisces.";
+}
+if(date<=20 && month=="march")
+{
+cout<<"Your Zodiac sign is Pisces.";
+}
+
+}
